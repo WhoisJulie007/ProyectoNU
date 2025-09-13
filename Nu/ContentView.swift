7f36a19e98dcbd5f6c2c15ad3f38972c9d024ae5
Julia <maydeli.castan@iest.edu.mx>
2025-09-12 18:14:17 -0600

    Initial Commit

---
+++ Nu/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Nu
+//
+//  Created by WIN603 on 12/09/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Nu/NuApp.swift
@@ -0,0 +1,17 @@
+//
+//  NuApp.swift
+//  Nu
+//
+//  Created by WIN603 on 12/09/25.
+//
+
+import SwiftUI
+
+@main
+struct NuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
